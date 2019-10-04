763c2819e753c982708bbc611e74e72f6432d113
Ahmad <ahmad2smile@protonmail.com>
2019-10-04 15:17:58 +0700

    Add project files.

---
+++ Orion/Orion/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Orion.Models;
+
+namespace Orion.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
