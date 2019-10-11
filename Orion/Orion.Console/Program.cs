50caceb961215c30dc4ab17b263e6110bafb4329
Ahmad <ahmad2smile@protonmail.com>
2019-10-11 17:23:32 +0700

    Added Test Console proj

---
+++ Orion/Orion.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Orion.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
