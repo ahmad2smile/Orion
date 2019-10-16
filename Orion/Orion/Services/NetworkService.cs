538732507d7e1fe9c25698957506579d1f92b578
Ahmad <ahmad2smile@protonmail.com>
2019-10-16 17:25:19 +0700

    moved to net-dns dependency

--- Orion/Orion/Services/NetworkService.cs
+++ Orion/Orion/Services/NetworkService.cs
@@ -1,7 +1,12 @@
 ﻿using Makaretu.Dns;
 using Orion.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace Orion.Services
 {
@@ -9,29 +14,101 @@ namespace Orion.Services
     {
         private const string OrionDomain = "orion.p2p.app.local";
 
-        private MulticastService MulticastService { get; set; } = new MulticastService();
+        private const int MulticastPort = 5353;
+        private static readonly IPAddress MulticastAddressIp4 = IPAddress.Parse("224.0.0.251");
 
         public delegate void FoundNode(Node node);
 
         public event FoundNode FoundNodeEvent;
 
+        public event EventHandler<UdpReceiveResult> MessageReceived;
+
+        private static IEnumerable<NetworkInterface> GetNetworkInterfaces()
+        {
+            var allUpNetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .ToArray();
+
+            var nonLoopBackInterfaces = allUpNetworkInterfaces
+            .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .ToArray();
+
+            return nonLoopBackInterfaces.Length > 0 ? nonLoopBackInterfaces : allUpNetworkInterfaces;
+        }
+
+        private IEnumerable<IPAddress> GetNetworkInterfaceLocalAddresses()
+        {
+            return GetNetworkInterfaces().SelectMany(nic => nic
+                    .GetIPProperties()
+                    .UnicastAddresses
+                    .Select(x => x.Address)
+                    .Where(x => x.AddressFamily != AddressFamily.InterNetworkV6 || x.IsIPv6LinkLocal))
+                .Where(a => a.AddressFamily == AddressFamily.InterNetwork);
+        }
+
         public void StartNetwork()
         {
             RegisterQueryListener();
 
-            MulticastService.Start();
+            var receiver = new UdpClient(AddressFamily.InterNetwork);
+
+            receiver.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            receiver.Client.Bind(new IPEndPoint(IPAddress.Any, MulticastPort));
+
+            var networkAddresses = GetNetworkInterfaceLocalAddresses();
+
+            foreach (var ipAddress in networkAddresses)
+            {
+                receiver.Client.SetSocketOption(
+                    SocketOptionLevel.IP,
+                    SocketOptionName.AddMembership,
+                    new MulticastOption(MulticastAddressIp4, ipAddress)
+                );
+            }
+
+            ListenForDnsMessages(receiver);
+        }
+
+        private void ListenForDnsMessages(UdpClient receiver)
+        {
+            Task.Run(async () =>
+            {
+                var task = receiver.ReceiveAsync();
+
+                await task.ContinueWith(x =>
+                        ListenForDnsMessages(receiver),
+                    TaskContinuationOptions.OnlyOnRanToCompletion |
+                    TaskContinuationOptions.RunContinuationsAsynchronously
+                );
+
+                await task.ContinueWith(x =>
+                        MessageReceived?.Invoke(this, x.Result),
+                    TaskContinuationOptions.OnlyOnRanToCompletion |
+                    TaskContinuationOptions.RunContinuationsAsynchronously
+                );
+
+                await task.ConfigureAwait(false);
+            });
         }
 
         private void RegisterQueryListener()
         {
-            MulticastService.QueryReceived += (sender, eventArgs) =>
+            MessageReceived += (sender, result) =>
             {
-                var question = eventArgs.Message.Questions.First();
+                var message = new Message();
+                message.Read(result.Buffer, 0, result.Buffer.Length);
+                if (message.Questions == null || message.Questions.Count == 0)
+                {
+                    return;
+                }
+
+                var question = message.Questions.First();
+
                 var domain = string.Join(".", question.Name.Labels.ToArray());
 
                 if (domain != OrionDomain) return;
 
-                var ip = eventArgs.RemoteEndPoint.Address.ToString();
+                var ip = result.RemoteEndPoint.Address.ToString();
 
                 var node = new Node
                 {
