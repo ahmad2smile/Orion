7925d6f7a20ebe14d59fd5e7ba18da897c7dfc6b
Ahmad <ahmad2smile@protonmail.com>
2019-10-20 03:35:25 +0700

    starting broadcast self node

--- Orion/Orion.Console/Program.cs
+++ Orion/Orion.Console/Program.cs
@@ -6,14 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        var networkService = new NetworkService();
-        networkService.StartNetwork();
+        var _networkService = new NetworkService();
+        _networkService.StartNetwork();
+        Console.WriteLine("============================================");
+        Console.WriteLine("Starting Network");
+        Console.WriteLine("============================================\n");
 
-        networkService.FoundNodeEvent += (Node node) =>
+        _networkService.FoundNodeEvent += (Node node) =>
         {
             Console.WriteLine($"Name: {node.Name}       IP: {node.Ip}");
-            
         };
+
+        _networkService.BroadcastSelfNode();
+        Console.WriteLine("============================================");
+        Console.WriteLine("Broadcasting Self Node");
+        Console.WriteLine("============================================\n");
 
         Console.ReadKey();
     }
